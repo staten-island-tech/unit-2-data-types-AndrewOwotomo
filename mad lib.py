d9ecce1ce4246ab5ab733074651d35046f0bfca5
AndrewOwotomo <Andrewo78@nycstudents.net>
2024-09-12 10:12:06 -0400

    YIPEEEE
    ada

--- mad lib.py
+++ mad lib.py
@@ -1,3 +1,22 @@
-x = 3
-y = float(3)
-print(x,y)
+# x = 3
+# y = float(3)
+# print(x,y)
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+#     print(values[0])
+# print(values[6])
+# x = "this is a thing"
+# y= x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+day_of_week = input("what day is it? ")
+if day_of_week == "Thursday":
+    print("correct")
+    print(f"Congrats it is {day_of_week}")
+else:
+    print("incorrect")
+    x = "test"
+    print(f"IT IS NOT {day_of_week}")
