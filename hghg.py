283d3eed10ee3ba7fe334a274ca30b61bea8d636
AndrewOwotomo <Andrewo78@nycstudents.net>
2024-09-25 09:52:42 -0400

    niaaaaa

--- hghg.py
+++ hghg.py
@@ -1,7 +1,9 @@
 number = int(input("Stick in number"))
 number2 = int(input("Stick in number"))
 common=[]
-def find_factors(x):
-    for i in range:
-        if number % x == 0 and number2 % x == 0:
-            common.append(x)
+def find_factors(number,number2):
+    for i in range(1,number+1):
+        if number % i == 0 and number2 % i == 0:
+            common.append(i)
+find_factors(number, number2)
+print(common[-1])
